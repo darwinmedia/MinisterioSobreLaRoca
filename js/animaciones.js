432e55b189a6f42933310a5a44da9dbd9651b33e
Darwin Audiomedia <67213583+darwinmedia@users.noreply.github.com>
2025-03-05 22:45:11 -0600

    f
    f

---
+++ js/animaciones.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+jQuery(document).ready(function() {
+
+	jQuery('.animar').addClass("oculto").viewportChecker({
+
+	    classToAdd: 'visible animated flip', // Clases que se agregan cuando el elemento es visible
+
+	    offset: 100    
+
+	   });   
+
+});   
+
+jQuery(document).ready(function() {
+
+	jQuery('.animar1').addClass("oculto").viewportChecker({
+
+	    classToAdd: 'visible animated pulse', // Clases que se agregan cuando el elemento es visible
+
+	    offset: 100    
+
+	   });   
+
+}); 
+
+jQuery(document).ready(function() {
+
+	jQuery('.animar2').addClass("oculto").viewportChecker({
+
+	    classToAdd: 'visible animated fadeIn', // Clases que se agregan cuando el elemento es visible
+
+	    offset: 100    
+
+	   });   
+
+}); 
